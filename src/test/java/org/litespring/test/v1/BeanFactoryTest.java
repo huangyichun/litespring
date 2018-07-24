37b8767c0d60934a5c266f4c88d4b4a07de44d22
huangyichun <huangyichun@jd.com>
2018-07-24 18:08:45 +0800

    添加异常测试方法

--- src/test/java/org/litespring/test/v1/BeanFactoryTest.java
+++ src/test/java/org/litespring/test/v1/BeanFactoryTest.java
@@ -1,7 +1,9 @@
 package org.litespring.test.v1;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.litespring.beans.BeanDefinition;
+import org.litespring.beans.factory.BeanCreationException;
 import org.litespring.beans.factory.BeanFactory;
 import org.litespring.beans.factory.support.DefaultBeanFactory;
 import org.litespring.service.v1.PetStoreService;
@@ -20,5 +22,16 @@ public class BeanFactoryTest {
         assertEquals("org.litespring.service.v1.PetStoreService", bd.getBeanClassName());
         PetStoreService petStoreService = (PetStoreService)factory.getBean("petStore");
         assertNotNull(petStoreService);
+    }
+
+    @Test
+    public void testInvalidBean(){
+        BeanFactory factory = new DefaultBeanFactory("petstore-v1.xml");
+        try {
+            factory.getBean("invalidBean");
+        } catch (BeanCreationException e) {
+            return;
+        }
+        Assert.fail("expect BeanCreationException ");
     }
 }
