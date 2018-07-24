e38bcec6fd8b92783f916bd9ce3f0598382e411b
huangyichun <huangyichun@jd.com>
2018-07-24 16:23:12 +0800

    创建DefaultBeanFactory，实现BeanFactory接口的两个方法

---
+++ src/main/java/org/litespring/beans/factory/support/DefaultBeanFactory.java
@@ -0,0 +1,80 @@
+package org.litespring.beans.factory.support;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.litespring.beans.BeanDefinition;
+import org.litespring.beans.factory.BeanFactory;
+import org.litespring.util.ClassUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DefaultBeanFactory implements BeanFactory {
+
+    public static final String ID_ATTRIBUTE = "id";
+    public static final String CLASS_ATTRIBUTE = "class";
+
+    private final Map<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<String, BeanDefinition>();
+
+    public DefaultBeanFactory(String configFile) {
+        loadBeanDefinition(configFile);
+    }
+
+    private void loadBeanDefinition(String configFile){
+        InputStream is = null;
+        try {
+            ClassLoader cl = ClassUtils.getDefaultClassLoader();
+            is = cl.getResourceAsStream(configFile);
+            SAXReader reader = new SAXReader();
+            Document doc = reader.read(is);
+            Element root = doc.getRootElement();//<beans>
+            Iterator<Element>  iterator = root.elementIterator();
+            while(iterator.hasNext()){
+                Element ele = iterator.next();
+                String id = ele.attributeValue(ID_ATTRIBUTE);
+                String beanClassName = ele.attributeValue(CLASS_ATTRIBUTE);
+                BeanDefinition bd = new GenericBeanDeanDefinition(id, beanClassName);
+                this.beanDefinitionMap.put(id, bd);
+            }
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }finally {
+            if(is != null){
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public BeanDefinition getBeanDefinition(String beanId) {
+        return this.beanDefinitionMap.get(beanId);
+    }
+
+    public Object getBean(String beanId) {
+        BeanDefinition bd = this.getBeanDefinition(beanId);
+        if(bd == null){
+            return null;
+        }
+        ClassLoader cl = ClassUtils.getDefaultClassLoader();
+        String beanClassName = bd.getBeanClassName();
+        try {
+            Class<?> clz = cl.loadClass(beanClassName);
+            return clz.newInstance();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
