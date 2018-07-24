06412ee3c5d3c368d0612c7788a2d8dfaf763c89
huangyichun <576305972@qq.com>
2018-07-24 22:49:51 +0800

    创建BeanDefinitionRegistry

---
+++ src/main/java/org/litespring/beans/factory/support/BeanDefinitionRegistry.java
@@ -0,0 +1,10 @@
+package org.litespring.beans.factory.support;
+
+import org.litespring.beans.BeanDefinition;
+
+public interface BeanDefinitionRegistry {
+
+    BeanDefinition getBeanDefinition(String beanId);
+
+    void registerBeanDefinition(String beanId, BeanDefinition bd);
+}
