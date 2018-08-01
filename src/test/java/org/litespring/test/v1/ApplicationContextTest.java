379a44cfb425faf047ef8d2e44bdefa428eca87e
huangyichun <huangyichun@jd.com>
2018-08-01 12:45:32 +0800

    添加testGetBeanFromFileSystemContext方法

--- src/test/java/org/litespring/test/v1/ApplicationContextTest.java
+++ src/test/java/org/litespring/test/v1/ApplicationContextTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.litespring.context.ApplicationContext;
 import org.litespring.context.support.ClassPathXmlApplicationContext;
+import org.litespring.context.support.FileSystemXmlApplicationContext;
 import org.litespring.service.v1.PetStoreService;
 
 public class ApplicationContextTest {
@@ -12,6 +13,13 @@ public class ApplicationContextTest {
     public void testGetBean(){
         ApplicationContext ctx = new ClassPathXmlApplicationContext("petstore-v1.xml");
         PetStoreService petStore = (PetStoreService)ctx.getBean("petStore");
+        Assert.assertNotNull(petStore);
+    }
+
+    @Test
+    public void testGetBeanFromFileSystemContext(){
+        ApplicationContext ctx = new FileSystemXmlApplicationContext("D:\\IdeaProject\\litespring\\src\\test\\resources\\petstore-v1.xml");
+        PetStoreService petStore = (PetStoreService) ctx.getBean("petStore");
         Assert.assertNotNull(petStore);
     }
 }
