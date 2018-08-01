31f7d00e1fba7bd811dbbc7f43598d031885c3cd
huangyichun <huangyichun@jd.com>
2018-08-01 14:45:02 +0800

    添加ClassLoader属性

--- src/main/java/org/litespring/context/support/AbstractApplicationContext.java
+++ src/main/java/org/litespring/context/support/AbstractApplicationContext.java
@@ -4,23 +4,35 @@ import org.litespring.beans.factory.support.DefaultBeanFactory;
 import org.litespring.beans.factory.xml.XmlBeanDefinitionReader;
 import org.litespring.context.ApplicationContext;
 import org.litespring.core.io.Resource;
+import org.litespring.util.ClassUtils;
 
 public abstract class AbstractApplicationContext implements ApplicationContext {
 
     private DefaultBeanFactory factory;
+    private ClassLoader beanClassLoader;
 
     public AbstractApplicationContext(String configFile) {
         factory = new DefaultBeanFactory();
         XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
         Resource resource = getResourceByPath(configFile);
         reader.loadBeanDefinition(resource);
+        factory.setBeanClassLoader(this.getBeanClassLoader());
     }
 
     protected abstract Resource getResourceByPath(String configFile);
 
-
     @Override
     public Object getBean(String beanId) {
         return factory.getBean(beanId);
+    }
+
+    @Override
+    public void setBeanClassLoader(ClassLoader beanClassLoader) {
+        this.beanClassLoader = beanClassLoader;
+    }
+
+    @Override
+    public ClassLoader getBeanClassLoader() {
+        return (this.beanClassLoader != null) ? this.beanClassLoader : ClassUtils.getDefaultClassLoader();
     }
 }
