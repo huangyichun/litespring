778c3f15dd8f2f8fbfdac59f5b9e7526982b5c8d
huangyichun <huangyichun@jd.com>
2018-08-01 17:40:19 +0800

    加入Bean的Scope字段和方法

--- src/main/java/org/litespring/beans/BeanDefinition.java
+++ src/main/java/org/litespring/beans/BeanDefinition.java
@@ -2,5 +2,13 @@ package org.litespring.beans;
 
 public interface BeanDefinition {
 
+    String SCOPE_SINGLETON = "singleton";
+    String SCOPE_PROTOTYPE = "prototype";
+    String SCOPE_DEFAULT = "";
+
+    boolean isSingleton();
+    boolean isPrototype();
+    String getScope();
+    void setScope(String scope);
     String getBeanClassName();
 }
