cbb0f495855daffe96f6dc07bca2d49e6755e7f0
huangyichun <huangyichun@jd.com>
2018-08-01 14:25:45 +0800

    删除无用包

--- src/main/java/org/litespring/context/support/AbstractApplicationContext.java
+++ src/main/java/org/litespring/context/support/AbstractApplicationContext.java
@@ -3,7 +3,6 @@ package org.litespring.context.support;
 import org.litespring.beans.factory.support.DefaultBeanFactory;
 import org.litespring.beans.factory.xml.XmlBeanDefinitionReader;
 import org.litespring.context.ApplicationContext;
-import org.litespring.core.io.ClassPathResource;
 import org.litespring.core.io.Resource;
 
 public abstract class AbstractApplicationContext implements ApplicationContext {
