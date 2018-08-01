ed9accb02534d4a2e816ed6e63f89e929bdcda29
huangyichun <huangyichun@jd.com>
2018-08-01 11:13:25 +0800

    添加Resource接口

---
+++ src/main/java/org/litespring/core/io/Resource.java
@@ -0,0 +1,10 @@
+package org.litespring.core.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public interface Resource {
+
+    InputStream getInputStream() throws IOException;
+    String getDescription();
+}
