3219c8ef63abef14d1c9087b4c5b31bad9068bef
huangyichun <huangyichun@jd.com>
2018-08-01 14:09:29 +0800

    封装ClassPathXmlApplication与FileSystemXmlApplicationContext共同部分

--- src/main/java/org/litespring/context/support/ClassPathXmlApplicationContext.java
+++ src/main/java/org/litespring/context/support/ClassPathXmlApplicationContext.java
@@ -1,24 +1,16 @@
 package org.litespring.context.support;
 
-import org.litespring.beans.factory.support.DefaultBeanFactory;
-import org.litespring.beans.factory.xml.XmlBeanDefinitionReader;
-import org.litespring.context.ApplicationContext;
 import org.litespring.core.io.ClassPathResource;
 import org.litespring.core.io.Resource;
 
-public class ClassPathXmlApplicationContext implements ApplicationContext {
-
-    private DefaultBeanFactory factory;
+public class ClassPathXmlApplicationContext extends AbstractApplicationContext {
 
     public ClassPathXmlApplicationContext(String configFile) {
-        factory = new DefaultBeanFactory();
-        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
-        Resource resource = new ClassPathResource(configFile);
-        reader.loadBeanDefinition(resource);
+        super(configFile);
     }
 
     @Override
-    public Object getBean(String beanId) {
-        return factory.getBean(beanId);
+    protected Resource getResourceByPath(String configFile) {
+        return new ClassPathResource(configFile);
     }
 }
--- src/main/java/org/litespring/context/support/FileSystemXmlApplicationContext.java
+++ src/main/java/org/litespring/context/support/FileSystemXmlApplicationContext.java
@@ -1,23 +1,16 @@
 package org.litespring.context.support;
 
-import org.litespring.beans.factory.support.DefaultBeanFactory;
-import org.litespring.beans.factory.xml.XmlBeanDefinitionReader;
-import org.litespring.context.ApplicationContext;
 import org.litespring.core.io.FileSystemResource;
 import org.litespring.core.io.Resource;
 
-public class FileSystemXmlApplicationContext implements ApplicationContext {
-    private DefaultBeanFactory factory;
+public class FileSystemXmlApplicationContext extends AbstractApplicationContext {
 
     public FileSystemXmlApplicationContext(String configFile) {
-        factory = new DefaultBeanFactory();
-        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
-        Resource resource = new FileSystemResource(configFile);
-        reader.loadBeanDefinition(resource);
+        super(configFile);
     }
 
     @Override
-    public Object getBean(String beanId) {
-        return factory.getBean(beanId);
+    protected Resource getResourceByPath(String configFile) {
+        return new FileSystemResource(configFile);
     }
 }
