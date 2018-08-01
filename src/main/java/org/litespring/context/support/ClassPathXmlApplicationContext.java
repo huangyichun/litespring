56b8dc84f97648a3d962dc934588233f17102630
huangyichun <huangyichun@jd.com>
2018-08-01 10:47:09 +0800

    创建ClassPathXmlApplicationContext

---
+++ src/main/java/org/litespring/context/support/ClassPathXmlApplicationContext.java
@@ -0,0 +1,21 @@
+package org.litespring.context.support;
+
+import org.litespring.beans.factory.support.DefaultBeanFactory;
+import org.litespring.beans.factory.xml.XmlBeanDefinitionReader;
+import org.litespring.context.ApplicationContext;
+
+public class ClassPathXmlApplicationContext implements ApplicationContext {
+
+    private DefaultBeanFactory factory;
+
+    public ClassPathXmlApplicationContext(String configFile) {
+        factory = new DefaultBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+        reader.loadBeanDefinition(configFile);
+    }
+
+    @Override
+    public Object getBean(String beanId) {
+        return factory.getBean(beanId);
+    }
+}
