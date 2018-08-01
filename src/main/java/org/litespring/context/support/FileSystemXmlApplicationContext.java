f46aa7f5b89ada8c746b9b08659f52bc9022eebe
huangyichun <huangyichun@jd.com>
2018-08-01 12:45:07 +0800

    创建FileSystemXmlApplication类

---
+++ src/main/java/org/litespring/context/support/FileSystemXmlApplicationContext.java
@@ -0,0 +1,23 @@
+package org.litespring.context.support;
+
+import org.litespring.beans.factory.support.DefaultBeanFactory;
+import org.litespring.beans.factory.xml.XmlBeanDefinitionReader;
+import org.litespring.context.ApplicationContext;
+import org.litespring.core.io.FileSystemResource;
+import org.litespring.core.io.Resource;
+
+public class FileSystemXmlApplicationContext implements ApplicationContext {
+    private DefaultBeanFactory factory;
+
+    public FileSystemXmlApplicationContext(String configFile) {
+        factory = new DefaultBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+        Resource resource = new FileSystemResource(configFile);
+        reader.loadBeanDefinition(resource);
+    }
+
+    @Override
+    public Object getBean(String beanId) {
+        return factory.getBean(beanId);
+    }
+}
