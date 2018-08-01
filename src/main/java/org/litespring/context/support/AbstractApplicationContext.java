406332be16ff04f30bc1f49ba5f775cc6184fc97
huangyichun <huangyichun@jd.com>
2018-08-01 14:08:33 +0800

    创建AbstractApplicationContext

---
+++ src/main/java/org/litespring/context/support/AbstractApplicationContext.java
@@ -0,0 +1,27 @@
+package org.litespring.context.support;
+
+import org.litespring.beans.factory.support.DefaultBeanFactory;
+import org.litespring.beans.factory.xml.XmlBeanDefinitionReader;
+import org.litespring.context.ApplicationContext;
+import org.litespring.core.io.ClassPathResource;
+import org.litespring.core.io.Resource;
+
+public abstract class AbstractApplicationContext implements ApplicationContext {
+
+    private DefaultBeanFactory factory;
+
+    public AbstractApplicationContext(String configFile) {
+        factory = new DefaultBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+        Resource resource = getResourceByPath(configFile);
+        reader.loadBeanDefinition(resource);
+    }
+
+    protected abstract Resource getResourceByPath(String configFile);
+
+
+    @Override
+    public Object getBean(String beanId) {
+        return factory.getBean(beanId);
+    }
+}
